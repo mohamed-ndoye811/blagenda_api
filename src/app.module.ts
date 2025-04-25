13931cb6db972b21bc567d0c4d4e402e1894d38e
Mohamed Ndoye <mohamed@diamondy.dev>
2025-04-25 12:21:04 +0200

    fix(app): Remove unused services and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 
 @Module({
 	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
